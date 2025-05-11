8c4b0b55a6f4a5a1e37931fd331e0ee40f6407a9
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-05-11 13:53:49 +0200

    fixtures implementation

---
+++ frontend/src/components/fixture/FixtureItem.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from 'react-router-dom';
+import './FixtureItem.css';
+
+const FixtureItem = ({ fixture }) => {
+    return (
+        <li className="fixture-list__item">
+            <p className="fixture-list__number">{fixture.date}</p>
+            <section className="fixture-list__details">
+                <NavLink>{fixture.homeTeam}</NavLink>
+                <NavLink>versus</NavLink>
+                <NavLink>{fixture.awayTeam}</NavLink>
+            </section>
+            <p className="fixture-list__date">{fixture.time}</p>
+        </li>
+    );
+};
+
+export default FixtureItem;
--- frontend/src/components/fixture/FixtureList.jsx
+++ frontend/src/components/fixture/FixtureList.jsx
@@ -1,5 +1,56 @@
+import FixtureItem from './FixtureItem';
+import './FixtureList.css';
+
 const FixtureList = () => {
-    return <h2>Fixture List</h2>;
+    const fixtures = [
+        {
+            id: 'f1',
+            number: '#123',
+            awayTeam: 'Team A',
+            homeTeam: 'Team B',
+            date: '10-01-2024',
+            time: '15:00',
+            venue: 'All nation field',
+            field: 'Field A',
+        },
+        {
+            id: 'f2',
+            number: '#123',
+            awayTeam: 'Team A',
+            homeTeam: 'Team B',
+            date: '10-01-2024',
+            time: '15:00',
+            venue: 'All nation field',
+            field: 'Field A',
+        },
+        {
+            id: 'f3',
+            number: '#123',
+            awayTeam: 'Team A',
+            homeTeam: 'Team B',
+            date: '10-01-2024',
+            time: '15:00',
+            venue: 'All nation field',
+            field: 'Field A',
+        },
+        {
+            id: 'f4',
+            number: '#123',
+            awayTeam: 'Team A',
+            homeTeam: 'Team B',
+            date: '10-01-2024',
+            time: '15:00',
+            venue: 'All nation field',
+            field: 'Field A',
+        },
+    ];
+    return (
+        <ul className="fixture-list">
+            {fixtures.map((fixture) => (
+                <FixtureItem key={fixture.id} fixture={fixture} />
+            ))}
+        </ul>
+    );
 };
 
 export default FixtureList;
