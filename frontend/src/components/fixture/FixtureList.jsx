df5ca5af364f7b6d9a78996900f4b4d16e41efa4
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-05-13 21:54:04 +0200

    handling cases for when there are no results and fixtures

--- frontend/src/components/fixture/FixtureList.jsx
+++ frontend/src/components/fixture/FixtureList.jsx
@@ -1,12 +1,28 @@
+import Button from '../../utils/Button';
 import FixtureItem from './FixtureItem';
 import './FixtureList.css';
 
 const FixtureList = ({ fixtures }) => {
+    const handleGenerateFixtures = () => {
+        // verify the league creator and generate league fixtures if there are no results
+    };
+
     return (
         <ul className="fixture-list">
-            {fixtures.map((fixture) => (
-                <FixtureItem key={fixture.id} {...fixture} />
-            ))}
+            {fixtures.length !== 0 &&
+                fixtures.map((fixture) => (
+                    <FixtureItem key={fixture.id} {...fixture} />
+                ))}
+            {fixtures.length === 0 && (
+                <li className="no-fixtures">
+                    <p>No fixtures found</p>
+                    <div>
+                        <Button onClick={handleGenerateFixtures}>
+                            Generate
+                        </Button>
+                    </div>
+                </li>
+            )}
         </ul>
     );
 };
--- frontend/src/components/result/ResultList.jsx
+++ frontend/src/components/result/ResultList.jsx
@@ -1,12 +1,26 @@
-import ResultItem from './ResultItem';
+import ResultItem from './ResultItem.jsx';
+import Button from '../../utils/Button.jsx';
 import './ResultList.css';
 
 const ResultList = ({ results }) => {
+    const handleAddResults = () => {
+        // navigate to the add results page
+    };
+
     return (
         <ul className="result-list">
-            {results.map((result) => (
-                <ResultItem key={result.id} {...result} />
-            ))}
+            {results.length !== 0 &&
+                results.map((result) => (
+                    <ResultItem key={result.id} {...result} />
+                ))}
+            {results.length === 0 && (
+                <li className="no-results">
+                    <p>No results found</p>
+                    <div>
+                        <Button onClick={handleAddResults}>Add</Button>
+                    </div>
+                </li>
+            )}
         </ul>
     );
 };
