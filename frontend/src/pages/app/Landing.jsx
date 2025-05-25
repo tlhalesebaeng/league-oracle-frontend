cf5f320ca0a642d0cc75b72dfa3263ce85860c69
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-05-25 10:55:42 +0200

    navigating to the home page if we are authenticated

--- frontend/src/pages/app/Landing.jsx
+++ frontend/src/pages/app/Landing.jsx
@@ -1,13 +1,17 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 import { uiActions } from '../../store/ui/ui-slice.js';
 
 const Landing = () => {
+    const isAuth = useSelector((state) => state.auth.isAuthenticated);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(uiActions.showAuthButtons()); // show the login and get started buttons
     }, []);
+
+    if (isAuth) return <Navigate to="/home" />;
 
     return (
         <main>
