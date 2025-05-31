1399553c77d603611f43d9bab58bd8e4e38ca6bb
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-05-31 18:54:07 +0200

    handling promise rejections

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -22,29 +22,34 @@ export function useFetch() {
     const [isLoading, setIsLoading] = useState(false);
 
     async function request(url, method, data = {}, options = {}) {
-        setIsLoading(true);
-        const response = await functions[method](url, data, options);
-        setIsLoading(false);
+        try {
+            setIsLoading(true);
+            const response = await functions[method](url, data, options);
+            setIsLoading(false);
 
-        if (response.status === 404) {
-            // the url we are sending a request to is not found (developer error)
-            setError('An error occurred');
-            return undefined;
-        }
-
-        if (response.status < 200 || response.status >= 300) {
-            // set the error
-            const responseData = response.data;
-            if (responseData) {
-                setError(responseData.message);
-            } else {
-                setError('Something went wrong! Please try again later');
+            if (response.status === 404) {
+                // the url we are sending a request to is not found (developer error)
+                setError('An error occurred');
+                return undefined;
             }
 
-            return undefined;
-        }
+            if (response.status < 200 || response.status >= 300) {
+                // set the error
+                const responseData = response.data;
+                if (responseData) {
+                    setError(responseData.message);
+                } else {
+                    setError('Something went wrong! Please try again later');
+                }
 
-        return response;
+                return undefined;
+            }
+
+            return response;
+        } catch (error) {
+            setIsLoading(false);
+            setError('Something went wrong! Please try again later');
+        }
     }
 
     return { isLoading, error, request, setError };
