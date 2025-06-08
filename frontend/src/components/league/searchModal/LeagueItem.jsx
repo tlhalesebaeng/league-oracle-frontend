c302d02bf857f564a2c488bb16aa50af7043c89f
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-06-08 13:55:05 +0200

    removing function definition from each child

--- frontend/src/components/league/searchModal/LeagueItem.jsx
+++ frontend/src/components/league/searchModal/LeagueItem.jsx
@@ -1,22 +1,8 @@
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { uiActions } from '../../../store/ui/ui-slice.js';
-
 import './LeagueItem.css';
 
-const LeagueItem = ({ id, name, dateCreated }) => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-
-    const handleShowLeague = () => {
-        dispatch(uiActions.hideLeaguesModal());
-        navigate(`/leagues/${id}`, {
-            state: { id, leagueName: name, dateCreated },
-        });
-    };
-
+const LeagueItem = ({ name, dateCreated, onShowLeague }) => {
     return (
-        <li onClick={handleShowLeague} className="league-list__item">
+        <li onClick={onShowLeague} className="league-list__item">
             <p>{name}</p>
             <p>{dateCreated}</p>
         </li>
--- frontend/src/components/league/searchModal/LeagueList.jsx
+++ frontend/src/components/league/searchModal/LeagueList.jsx
@@ -1,15 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../../store/ui/ui-slice.js';
 import LeagueItem from './LeagueItem.jsx';
 import './LeagueList.css';
 
 const LeagueList = ({ leagues }) => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleShowLeague = (id) => {
+        dispatch(uiActions.hideLeaguesModal());
+        navigate(`/leagues/${id}`);
+    };
+
     return (
         <ul className="league-list">
             {leagues.map((league) => (
                 <LeagueItem
                     key={league._id}
-                    id={league._id}
                     name={league.name}
                     dateCreated={league.created}
+                    onShowLeague={() => handleShowLeague(league._id)}
                 />
             ))}
         </ul>
