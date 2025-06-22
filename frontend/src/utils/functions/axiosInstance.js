7c0a19d0a43ead4e74c97c3c8f079d000ba5d941
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-06-22 23:02:58 +0200

    fix the backend base url environment name

--- frontend/src/utils/functions/axiosInstance.js
+++ frontend/src/utils/functions/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_BASEURL,
+    baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
     validateStatus: () => {
         return true; // resolve all responses
     },
