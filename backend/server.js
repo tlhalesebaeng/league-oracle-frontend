0846e9b5a4d484bc0756184ae422b66016bd73e6
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-06-25 16:45:37 +0200

    shut down the server on unhandled rejection or uncaught exception

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,11 @@ import connectDB from './src/utils/connectDB.js';
 // Get the port
 const PORT = process.env.PORT;
 
+// Get the server environment
+const SERVER_ENV = process.env.SERVER_ENV;
+
 // Start the server
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     // Log to the console to show that we have connected successfully
     console.log(`Listening on port ${PORT}...`);
 
@@ -15,6 +18,26 @@ app.listen(PORT, () => {
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (error) => {
-    // Log the error to the console
-    console.log('ERROR:', error.name, error.message);
+    if (SERVER_ENV !== 'production') {
+        // Log the error to the console
+        console.log('ERROR:', error.name, error.message);
+    }
+
+    server.close(() => {
+        console.log('Shutting down the server...');
+        process.exit(1);
+    });
+});
+
+// Handle uncaught exceptions
+process.on('uncaughtException', (error) => {
+    if (SERVER_ENV !== 'production') {
+        // Log the error to the console
+        console.log('ERROR:', error.name, error.message);
+    }
+
+    server.close(() => {
+        console.log('Shutting down the server...');
+        process.exit(1);
+    });
 });
