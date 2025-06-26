232c1946f5fb5ea08fcbc611c596558c8b82abfc
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-06-26 15:47:40 +0200

    use plus icon instead of svg

--- frontend/src/components/navigation/NavigationLinks.jsx
+++ frontend/src/components/navigation/NavigationLinks.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { useLogout } from '../../hooks/useLogout.js';
 
 import Logout from '../app/Logout.jsx';
+import plusImg from '../../assets/plus.png';
 import './NavigationLinks.css';
 
 const NavigationLinks = () => {
@@ -18,16 +19,7 @@ const NavigationLinks = () => {
                 onClick={handleCreateLeague}
                 className="navigation-links__create"
             >
-                <svg
-                    aria-hidden="true"
-                    height="25"
-                    viewBox="0 0 16 16"
-                    version="1.1"
-                    width="25"
-                    data-view-component="true"
-                >
-                    <path d="M7.75 2a.75.75 0 0 1 .75.75V7h4.25a.75.75 0 0 1 0 1.5H8.5v4.25a.75.75 0 0 1-1.5 0V8.5H2.75a.75.75 0 0 1 0-1.5H7V2.75A.75.75 0 0 1 7.75 2Z"></path>
-                </svg>
+                <img src={plusImg} />
             </div>
             <Logout onLogout={handleLogout} loading={isLoading} />
         </li>
