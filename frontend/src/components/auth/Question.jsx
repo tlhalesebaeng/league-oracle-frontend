1d3df7bca6b7cc558c88593acff78fa2fccc36d8
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-06-29 20:53:07 +0200

    use link instead of navlink

--- frontend/src/components/auth/Question.jsx
+++ frontend/src/components/auth/Question.jsx
@@ -1,11 +1,11 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Question.css';
 
 const Question = ({ link, linkText, children }) => {
     return (
         <section className="question">
             <p>{children}</p>
-            <NavLink to={link}>{linkText}</NavLink>
+            <Link to={link}>{linkText}</Link>
         </section>
     );
 };
