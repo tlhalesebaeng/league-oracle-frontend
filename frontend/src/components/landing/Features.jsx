d910741905bdb64667eac850fa71c7af46be3c1f
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-06-30 13:52:25 +0200

    make features component representational

--- frontend/src/components/landing/Features.jsx
+++ frontend/src/components/landing/Features.jsx
@@ -1,33 +1,7 @@
 import Card from '../app/Card.jsx';
-import calendarImg from '../../assets/calendar.png';
-import resultImg from '../../assets/result.png';
-import leagueImg from '../../assets/league.png';
 import './Features.css';
 
-const Features = () => {
-    const features = [
-        {
-            _id: 'f1',
-            imgSrc: leagueImg,
-            title: 'League Management',
-            description:
-                'Create leagues with any amount of teams, update your leagues and browse other league with ease',
-        },
-        {
-            _id: 'f2',
-            imgSrc: calendarImg,
-            title: 'Fixture Handling',
-            description:
-                'Generate home and away season fixtures for your league, view league fixtures and easily update fixture details ',
-        },
-        {
-            _id: 'f3',
-            imgSrc: resultImg,
-            title: 'Results',
-            description:
-                'Simplified way to add fixture result, view league results and easily make update to your results with little effort',
-        },
-    ];
+const Features = ({ featuresList }) => {
     return (
         <div className="features">
             <h2>Core Features</h2>
@@ -36,7 +10,7 @@ const Features = () => {
                 to get started with us to use all this great features
             </p>
             <ul>
-                {features.map((feature) => (
+                {featuresList.map((feature) => (
                     <Card key={feature._id}>
                         <li>
                             <img src={feature.imgSrc} />
