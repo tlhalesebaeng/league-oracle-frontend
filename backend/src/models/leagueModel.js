1d450c102737086d5fca9b3421e41e335be6109d
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-07-01 16:08:08 +0200

    add timestamps to the model

--- backend/src/models/leagueModel.js
+++ backend/src/models/leagueModel.js
@@ -2,19 +2,25 @@ import { Schema, model } from 'mongoose';
 import teamSchema from './teamModel.js';
 import AppError from '../AppError.js';
 
-const leagueSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, 'Name required! Please provide a league name.'],
-        unique: true,
+const leagueSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Name required! Please provide a league name.'],
+            unique: true,
+        },
+        creator: {
+            type: Schema.ObjectId,
+            ref: 'User',
+            required: [
+                true,
+                'Creator required! Please provide a league creator ',
+            ],
+        },
+        teams: [teamSchema],
     },
-    creator: {
-        type: Schema.ObjectId,
-        ref: 'User',
-        required: [true, 'Creator required! Please provide a league creator '],
-    },
-    teams: [teamSchema],
-});
+    { timestamps: true }
+);
 
 // ensure that teams have unique name within a league
 leagueSchema.pre('save', function (next) {
