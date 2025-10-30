9fdfd6ab8220d7526ae259ec058bca04ed4dd839
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-30 21:28:26 +0200

    Delete node backend

--- backend/src/utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-// function to handle promise rejections instead of using try and catch blocks
-const asyncHandler = (func) => {
-    return (req, res, next) => {
-        func(req, res, next).catch(next);
-    };
-};
-
-export default asyncHandler;
