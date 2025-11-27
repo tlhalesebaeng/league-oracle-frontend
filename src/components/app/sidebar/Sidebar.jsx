90b709a4102983274f65d51d454b25b0bb53dbd1
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-27 11:30:34 +0200

    Remove loading with useEffect

--- src/components/app/sidebar/Sidebar.jsx
+++ src/components/app/sidebar/Sidebar.jsx
@@ -1,40 +1,22 @@
-import { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
-import { useFetch } from '../../../hooks/useFetch.js';
 import { sidebarActions } from '../../../store/ui/sidebar-slice.js';
 import { useLogout } from '../../../hooks/useLogout.js';
 
 import Button from '../button/Button.jsx';
 import SidebarLinks from './SidebarLinks.jsx';
 import closeImg from '../../../assets/close.png';
-import './Sidebar.css';
 import Logout from '../logout/Logout.jsx';
 import MyLeagues from '../../../features/app/home/my-leagues/MyLeagues.jsx';
+import './Sidebar.css';
 
 const Sidebar = () => {
     const isAuth = useSelector((state) => state.auth.isAuthenticated);
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const { request, error, isLoading } = useFetch();
-    const [leagues, setLeagues] = useState([]);
-    const { handleLogout, isLoading: logoutLoading } = useLogout();
-
-    useEffect(() => {
-        if (error) {
-            dispatch(showAlert('error', 'Could not fetch your leagues'));
-            return;
-        }
-
-        const getMyLeague = async () => {
-            const response = await request('/leagues/mine', 'get');
-            if (response) setLeagues(response.data);
-        };
-
-        getMyLeague();
-    }, [error]);
+    const { handleLogout, isLoading } = useLogout();
 
     const resetBodyOverflow = () => {
         // This is not declarative and its not recommeded to do this, but i cant find a better easy way :(
@@ -72,17 +54,13 @@ const Sidebar = () => {
             </li>
             <li className="sidebar__content">
                 <SidebarLinks onNavigate={handleNavigation} />
-                <MyLeagues
-                    type="my-leagues__sidebar"
-                    loading={isLoading}
-                    leagues={leagues}
-                />
+                {isAuth && <MyLeagues type="my-leagues__sidebar" />}
             </li>
             <li className="sidebar__auth-buttons">
                 {isAuth && (
                     <Logout
                         onLogout={handleSidebarLogout}
-                        loading={logoutLoading}
+                        loading={isLoading}
                     />
                 )}
                 {!isAuth && (
